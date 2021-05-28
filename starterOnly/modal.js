c45dd556a29c69abbb1c12b894d14664133b0097
Fabrice <jrose.fab@gmail.com>
2021-05-28 14:06:03 +0200

    # 2 - add error message on field

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,6 +19,8 @@ const lastNameInput = document.getElementsByName("last")[0];
 const emailInput = document.getElementsByName("email")[0];
 const quantityInput = document.getElementsByName("quantity")[0];
 const cguCheckedInput = document.getElementById("checkbox1");
+const birthdateInput = document.getElementById("birthdate")[0];
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -34,37 +36,85 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-function isInputValid(input, type) {
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(
+    newNode,
+    referenceNode.nextElementSibling
+  );
+}
+
+function errorMessage(id, input) {
+  let message = document.createElement("p");
+  if (input.nextElementSibling == null) {
+    insertAfter(message, input);
+  }
+  message.classList.add("error");
+
+  switch (id !== null) {
+    case id == "first":
+      message.innerHTML =
+        "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+      break;
+    case id == "last":
+      message.innerHTML =
+        "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+      break;
+    case id == "email":
+      message.innerHTML = "Adresse email non valide";
+      break;
+    case id == "birthdate":
+      message.innerHTML = "Vous devez entrer votre date de naissance.";
+      break;
+    case id == "quantity":
+      message.innerHTML = "Veuillez saisir une donnée numérique";
+      break;
+
+    default:
+      message.innerHTML =
+        "Une erreure est survenue veuillez réessayer plus tard";
+  }
+}
+
+function isInputValid(input, type, id) {
   const isEmail = type == "email";
+  const isText = type == "text";
 
   input.addEventListener("input", function (e) {
     var value = e.target.value;
-    if (type == "text") {
+    if (isText) {
       if (value.length < 2) {
         input.classList.add("danger");
+        errorMessage(id, input);
       } else {
         input.classList.remove("danger");
+        if (input.nextElementSibling !== null) {
+          input.nextElementSibling.remove();
+        }
       }
     } else if (type !== "text") {
       var pattern = isEmail ? /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ : /^\d+$/;
       if (pattern.test(value)) {
         input.classList.remove("danger");
+        if (input.nextElementSibling !== null) {
+          input.nextElementSibling.remove();
+        }
       } else {
-        input.classList.add("danger");
+        console.log(id);
+        errorMessage(id, input);
       }
     }
   });
 }
 
-isInputValid(firstNameInput, "text");
-isInputValid(lastNameInput, "text");
-isInputValid(emailInput, "email");
-isInputValid(quantityInput, "number");
+isInputValid(firstNameInput, "text", "first");
+isInputValid(lastNameInput, "text", "last");
+isInputValid(emailInput, "email", "email");
+isInputValid(quantityInput, "number", "quantity");
 
 function validate(e) {
   e.preventDefault();
   if (!cguCheckedInput.checked) {
-    alert("yo tu dois sélectionner le truc");
+    return alert("yo tu dois sélectionner le truc");
   }
 }
 
