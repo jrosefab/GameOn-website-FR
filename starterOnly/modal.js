4014cdee12515fc3ce2696fb9042816ae395ddb8
Fabrice <jrose.fab@gmail.com>
2021-05-28 12:32:31 +0200

    #2 - implement form validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeModalBtn = document.getElementsByClassName("close");
+const closeModalBtn = document.getElementsByClassName("close")[0];
 
+// inputs
+const firstNameInput = document.getElementsByName("first")[0];
+const lastNameInput = document.getElementsByName("last")[0];
+const emailInput = document.getElementsByName("email")[0];
+const quantityInput = document.getElementsByName("quantity")[0];
+const cguCheckedInput = document.getElementById("checkbox1");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 //close modal event
-closeModalBtn[0].addEventListener("click", closeModal);
+closeModalBtn.addEventListener("click", closeModal);
 
 function closeModal() {
   modalbg.style.display = "none";
@@ -27,3 +33,39 @@ function closeModal() {
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+function isInputValid(input, type) {
+  const isEmail = type == "email";
+
+  input.addEventListener("input", function (e) {
+    var value = e.target.value;
+    if (type == "text") {
+      if (value.length < 2) {
+        input.classList.add("danger");
+      } else {
+        input.classList.remove("danger");
+      }
+    } else if (type !== "text") {
+      var pattern = isEmail ? /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ : /^\d+$/;
+      if (pattern.test(value)) {
+        input.classList.remove("danger");
+      } else {
+        input.classList.add("danger");
+      }
+    }
+  });
+}
+
+isInputValid(firstNameInput, "text");
+isInputValid(lastNameInput, "text");
+isInputValid(emailInput, "email");
+isInputValid(quantityInput, "number");
+
+function validate(e) {
+  e.preventDefault();
+  if (!cguCheckedInput.checked) {
+    alert("yo tu dois sélectionner le truc");
+  }
+}
+
+console.log(cguCheckedInput.checked);
